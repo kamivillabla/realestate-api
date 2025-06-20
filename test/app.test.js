32b29fc18b2f0321bfb1a9e95a7123bda639a10f
kamivillabla <kvillablancabaez@gmail.com>
2025-06-19 20:17:10 -0400

    test(api): force intentional failure to verify GitHub Actions

--- test/app.test.js
+++ test/app.test.js
@@ -4,10 +4,10 @@ const app = require('../src/app');
 describe('GET /api/inmuebles', () => {
   it('debería retornar una lista de inmuebles', async () => {
     const res = await request(app).get('/api/inmuebles');
-    expect(res.statusCode).toBe(200);
+    expect(res.statusCode).toBe(404);
     expect(Array.isArray(res.body)).toBe(true);
     expect(res.body.length).toBeGreaterThan(0);
     expect(res.body[0]).toHaveProperty('direccion');
-    expect(res.body[0]).toHaveProperty('precio');
+    expect(res.body[0]).toHaveProperty('ubi');
   });
 });
